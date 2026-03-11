4471bdc51b27707c65f1afe9e13a5f96318c7148
Yaswanth <pittayaswanth1204@gmail.com>
2026-03-11 16:26:24 +0530

    Refactor Graph class and add BFS/DFS methods

--- Moeule1_Array/transverse.java
+++ Moeule1_Array/transverse.java
@@ -1,31 +1,82 @@
 import java.util.LinkedList;
-
+import java.util.Stack;
 class Graph {
 
-    int V; // number of vertices
+    int V; // vertices
     LinkedList<Integer> adj[]; // adjacency list
 
-    @SuppressWarnings("unchecked")
     Graph(int v) {
-        V = v;
+        this.V = v;
 
         adj = new LinkedList[V];
 
         for (int i = 0; i < V; i++) {
-            adj[i] = new LinkedList<Integer>();
+            adj[i] = new LinkedList<>();
         }
     }
 
-    // function to add edge
     void addEdge(int v, int w) {
         adj[v].add(w);
     }
 
-    // function to print graph
+    void BFS(int s) {
+
+        boolean visited[] = new boolean[V];
+
+        LinkedList<Integer> q = new LinkedList<>();
+
+        visited[s] = true;
+        q.add(s);
+        System.out.print(" BFS  Breadth First search");
+        while (!q.isEmpty()) {
+
+            s = q.poll();
+            System.out.print(s + " ");
+            
+            for (int i = 0; i < adj[s].size(); i++) {
+
+                int node = adj[s].get(i);
+
+                if (!visited[node]) {
+                    visited[node] = true;
+                    q.add(node);
+                }
+            }
+        }
+    }
+    
+    
+        void DFS(int s) {
+
+        boolean visited[] = new boolean[V];
+
+        Stack<Integer> stk = new Stack<>();
+
+        visited[s] = true;
+        stk.push(s);
+        System.out.print(" DFS  Depth First search");
+
+        while (!stk.isEmpty()) {
+
+            s = stk.pop();
+            System.out.print(s + " ");
+
+            for (int i = adj[s].size()-1; i >=0; i--) {
+
+                int node = adj[s].get(i);
+
+                if (!visited[node]) {
+                    visited[node] = true;
+                    stk.add(node);
+                }
+            }
+        }
+    }
+
     void printGraph() {
         for (int i = 0; i < V; i++) {
 
-            System.out.print(i + "->");
+            System.out.print(i + " -> ");
 
             for (int j = 0; j < adj[i].size(); j++) {
                 System.out.print(adj[i].get(j) + " ");
@@ -36,11 +87,10 @@ class Graph {
     }
 }
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
 
-       
+        System.out.println("Try programiz.pro");
 
         Graph g = new Graph(4);
 
@@ -50,5 +100,10 @@ public class Main {
         g.addEdge(2, 3);
 
         g.printGraph();
+
+        System.out.print("BFS Traversal: ");
+        g.BFS(0);
+          System.out.print("DFS Traversal: ");
+        g.DFS(0);
     }
 }
